55378697b2ccf59eddc1787cd494c40c57395c03
thgab <thgab74@gmail.com>
2016-05-24 20:16:23 +0200

    client rename feature

--- app.js
+++ app.js
@@ -8,18 +8,38 @@ GPIO.setMode(led, GPIO.OUT, GPIO.FLOAT);
 GPIO.setMode(button, GPIO.IN, GPIO.PULLUP);
 GPIO.write(led, 0);
 
+var name = 'noname';
+var f = File.open('myname', 'r');
+if (f) {
+	name = f.read();
+	f.close();
+}
+
 var ws = new WebSocket(wsurl);
 
 ws.onopen = function (ev) {
 	GPIO.setISR(button, GPIO.NEGEDGE, send);
-	ws.send('blinkR');
+	ws.send('blinkR:' + name);
 };
 
 ws.onmessage = function (ev) {
-	if (ev.data == 'blink') {
+	var evdata = ev.data.split(':');
+	var evfunction = evdata.shift();
+	if (typeof blinkR[evfunction] === 'function') {
+		blinkR[evfunction](evdata);
+	}
+};
+
+var blinkR = {
+	blink: function () {
 		GPIO.write(led, 1);
 		var xt = setTimeout(function () {
 			GPIO.write(led, 0);
 		}, 500);
+	},
+	rename: function (name) {
+		var f = File.open('myname', 'w+');
+		f.write(name[0]);
+		f.close();
 	}
-};
+}
